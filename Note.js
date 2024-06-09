e300d39f3800f698693dfc9072d03e5e3732ccc0
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-06-09 19:09:33 +0530

    Note

---
+++ Note.js
@@ -0,0 +1,17 @@
+//----------Meta Char----------//
+
+// Single char                 Quantifiers            position
+                               
+// /d = 0-9                   * = 0 or more        ^  = beginning
+// /w = A-Z a-z 0-9           + = 1 or more        $  = end
+// /s = whitespace            ? = 0 or 1           /b = Word boundry
+// .  = Any Character          {min, max}
+//                                {n}
+
+
+//----------Character Class----------//
+
+// l[yi]nk             // link and lynk
+// \d{3}[-.]\d{3}\d{4} // 123-456-7890 and 123.456.7890 
+
+// \(?\d{3}[-.)]\d{3}[-.]\d{4} //(212)867-5509
