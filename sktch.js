796c86aaacf3eea25e24098ef9e032a664094b10
ArmanAgwan <120042307+ArmanAgwan@users.noreply.github.com>
2024-06-13 17:02:46 +0530

    sktch

--- sktch.js
+++ sktch.js
@@ -10,7 +10,7 @@ function setup() {
     submit = select("#submit");
     submit.mousePressed(newText);
 }
-
+/*
 function newText() {
     var s = textfield.value();
 
@@ -28,9 +28,10 @@ function newText() {
     for (var i = 0; i < matches.length; i++) {}
     // createP(r.test(s));
     createP(matches[i]);
-}
+}*/
 
 
+/*
 function newText(){
     var s = textfield.value();
     // var r = /[.,!?\s]+/g;
@@ -40,6 +41,26 @@ function newText(){
     for(var i =0 ;i<word.length;i++){
         createP(word[i] );
     }
+}*/
+
+
+function newText() {
+    var s = textfield.value();
+    var r = /\b\w+\b/g;
+
+    var newstring = s.replace(r, replacer);
+    console.log(newstring);
+    createP(newstring);
+}
+
+function replacer(match) {
+    // console.log(match);
+    if (match.length ==4) {
+        return match.toUpperCase();
+    } else {
+        return match;
+    }
+
 }
 
 //---console---//
